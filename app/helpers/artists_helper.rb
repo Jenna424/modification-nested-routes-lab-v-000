56fadb0f713b5c60e6eea7779e63d0b98863df34
github username <github email address>
2018-01-05 11:09:25 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,17 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist, song)
+    if params[:artist_id] # user edits song through nested routing at "/artists/:artist_id/songs/:id/edit"
+      artist.name
+    else # user edits song directly via "/songs/:id/edit", so we create dropdown of artist options
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+  end
 end
+
+# Remember: the nested :edit resource route in config/routes.rb
+# provided the route helper method #edit_artist_song_path(artist_id, song_id),
+# which returns the string URL path "/artists/:artist_id/songs/:id/edit"
+# where user edits song through nested routing
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show, :new, :edit]
   end
   resources :songs
 end
+
+# Due to nested resource routes, we now have access to:
+
+# route helper method #artist_songs_path(artist_id),
+# which returns the string URL path "/artists/:artist_id/songs"
+# mapped to the #index action (method) of SongsController
+# to display the index of all songs by (i.e. belonging to) a specific artist
+
+# route helper method #artist_song_path(artist_id, song_id)
+# which returns the string URL path "/artists/:artist_id/songs/:id"
+# mapped to the #show action (method) of SongsController
+# to show a specific song belonging to a specific artist
+
+# route helper method #new_artist_song_path(artist_id),
+# which returns the string URL path "/artists/:artist_id/songs/new"
+# mapped to the #new action (method) of SongsController
+# to create a new song instance automatically belonging to the artist instance
+# user had clicked Create New Song link on the artist show page,
+# which led to "/artists/:artist_id/songs/new" page displaying form to create a new song belonging to the artist
+# we know who the artist is
+
+# route helper method #edit_artist_song_path(artist_id, song_id),
+# which returns the string URL path "/artists/:artist_id/songs/:id/edit"
+# mapped to the #edit action (method) of SongsController
+# to edit a specific song by a specific artist
